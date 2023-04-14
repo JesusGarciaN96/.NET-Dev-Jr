b185b1d53951bd517f5df832874d717d66d63e01
Jesus Alejandro Garcia <garciacode96@gmail.com>
2023-04-13 21:59:19 -0600

    CRUD Local Data

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -15,16 +15,56 @@ namespace JrDev.Controllers
                 new Character { Id = 3, Nombre = "Daniela", Fuerza = 400, Oficio = RPG.Clero }
             };
 
-        [HttpGet("Characters")]
+        [HttpGet]
         public ActionResult<IEnumerable<Character>> GetAll()
         { 
             return Ok(Personajes);
         }
 
-        [HttpGet("Character/{id}")]
+        [HttpGet("{id}")]
         public ActionResult<Character> Get(int id)
         {
             return Ok(Personajes.FirstOrDefault(per => per.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<Character>> Post(Character Personaje)
+        {
+            if(Personaje != null)
+            {
+                Personajes.Add(Personaje);
+                return Ok(Personajes);
+            }
+            return NotFound();
+        }
+
+        [HttpPut]
+        public ActionResult<IEnumerable<Character>> Put(Character Personaje)
+        {
+            if (Personaje == null)
+            {
+                return NotFound();
+            }
+
+            int indicePersonaje = Personajes.FindIndex(per => per.Id == Personaje.Id);
+            if(indicePersonaje > 0)
+            {
+                Personajes[indicePersonaje] = Personaje;
+                return Ok(Personajes);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Character>> Delete(int id)
+        {
+            int indicePersonaje = Personajes.FindIndex(pr => pr.Id == id);
+            if(indicePersonaje > 0)
+            {
+                Personajes.RemoveAt(indicePersonaje);
+                return Ok(Personajes);
+            }
+            return NotFound();
         }
     }
 }
